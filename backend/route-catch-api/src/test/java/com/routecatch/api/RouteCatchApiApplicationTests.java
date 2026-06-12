110d9eefe7f1ecff5e4d14be17dfadaa7c3cc228
halfdimension <iamharshmudgil1263@gmail.com>
2026-06-12 14:38:16 +0530

    Add Spring Boot backend skeleton

---
+++ backend/route-catch-api/src/test/java/com/routecatch/api/RouteCatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routecatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteCatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
